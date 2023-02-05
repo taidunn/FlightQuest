6ab90db52f3f2de023490b2815c42513ca60a8f1
Tai Dunn <39572984+taidunn@users.noreply.github.com>
2023-02-04 19:08:51 -0500

    Syntax

--- questions.py
+++ questions.py
@@ -1,119 +1,89 @@
-#categories
+import pyinputplus as pyip
 
+# Categories
 romantic = 0
 thrillseeker = 0
 explorer = 0
 escapist = 0
 foodie = 0
 social = 0
-kids = 0
 
-#question 1
-def question1():
-    print("Who are you traveling with?")
-    print("A) Adult")
-    print("B) Kids")
-    print("C) Alone")
-    input1 = input()
-    if input1 == "B":
-        global kids
-        kids+=1
-    else:
-        global romantic, thrillseeker, explorer, escapist, foodie, social
-        romantic+=1
-        thrillseeker+=1
-        explorer+=1
-        escapist+=1
-        foodie+=1
-        social+=1
+# Question One
+input1 = pyip.inputYesNo("Do you like traveling with others? (Y/N) ", limit=4)
 
-#question 2
-def question2():
-    print("Are you a Foodie?")
-    print("A) Yes")
-    print("B) No")
-    input1 = input()
-    global romantic, thrillseeker, explorer, escapist, foodie, social
-    if input1 == "A":
-        romantic+=1
-        explorer+=1
-        escapist+=1
-        foodie+=2
-        social+=1
+if input1 == "yes":
+    social += 3
+    explorer += 2
+else:
+    thrillseeker += 0
 
-#question 3
-def question3():
-    print("Would you like a romantic getaway?")
-    print("A) Yes")
-    print("B) No")
-    input1 = input()
-    global romantic, thrillseeker, explorer, escapist, foodie, social
-    if input1 == "A":
-        romantic+=2
-        escapist+=1
-        social+=1
+#Question Two
+input1 = pyip.inputYesNo("Are you a foodie? (Y/N) ", limit=4)
 
-#question 4
-def question4():
-    print("Are you a risk taker?")
-    print("A) Yes")
-    print("B) No")
-    input1 = input()
-    global romantic, thrillseeker, explorer, escapist, foodie, social
-    if input1 == "A":
-        thrillseeker += 2
-        explorer += 1
+if input1 == "yes":
+    foodie+=2
+else: 
+    social+=1
+    romantic+=1
+    explorer+=1
+    escapist+=1
 
-#question 5
-def question5():
-    print("Would you like to see geographic landmarks?")
-    print("A) Yes")
-    print("B) No")
-    input1 = input()
-    global romantic, thrillseeker, explorer, escapist, foodie, social
-    if input1 == "A":
-        explorer += 2
-        thrillseeker += 1
-        escapist += 1
+#Question Three
+input1 = pyip.inputYesNo("Would you like a romantic getaway? (Y/N) ", limit=4)
 
-def question6():
-    print("Would you enjoy minimal social interaction?")
-    print("A) Yes")
-    print("B) No")
-    input1 = input()
-    global romantic, thrillseeker, explorer, escapist, foodie, social
-    if input1 == "A":
-        escapist += 2
-    else:
-        social += 2
+if input1 == "yes":
+    romantic+=2
+else:
+    escapist+=1
+    social+1
 
-def category():
-    #global romantic, thrillseeker, explorer, escapist, foodie, social
-    #categories = [romantic, thrillseeker, explorer, escapist, foodie, social]
-    #for i in categories:
-     #   largest = 0
-      #  if i>largest:
-            #largest=i
-    #chosencategory = categories.index(largest)
-    #print(chosencategory)
-    categories = {romantic: "romantic", thrillseeker: "thrillseeker" , explorer: "explore", escapist: "escapist" , foodie: "foodie" , social: "social"}
-    print(romantic, thrillseeker, explorer, escapist, foodie, social)
-    #categories.sort(reverse=True)
-    #print(categories[0])
+#Question Four
+input1 = pyip.inputYesNo("Are you a risk taker? (Y/N) ", limit=4)
 
-    print(categories.get(max(categories)))
-    max(categories)
-    print(categories)
+if input1 == "yes":
+    thrillseeker += 2
+else:
+    explorer += 1
+
+# Question Five
+input1 = pyip.inputYesNo("Are you a geographic landmarks? (Y/N) ", limit=4)
+
+if input1 == "yes":
+    explorer += 2
+else:
+    thrillseeker += 1
+    escapist += 1
+
+# Question Six
+input1 = pyip.inputYesNo("Would you enjoy minimal social interaction? (Y/N) ", limit=4)
+
+if input1 == "yes":
+    escapist += 2
+else:
+    explorer += 1
+    thrillseeker += 1
+
+# Question Seven
+input1 = pyip.inputYesNo("Have you ever been abroad? (Y/N) ", limit=4)
+
+if input1 == "yes":
+    explorer += 3
+    social += 1
+else:
+    thrillseeker += 1
+
+# Question Seven
+input1 = pyip.inputYesNo("Could you live in another country for the rest of your life? (Y/N) ", limit=4)
+
+if input1 == "yes":
+    escapist += 3
+    explorer += 2
+    thrillseeker += 1
+else:
+    foodie += 0
 
 
+categories = {romantic: "romantic", thrillseeker: "thrillseeker" , explorer: "explore", escapist: "escapist" , foodie: "foodie" , social: "social"}
+# print(romantic, thrillseeker, explorer, escapist, foodie, social)
 
-
-question1()
-question2()
-question3()
-question4()
-question5()
-question6()
-category()
-
-
+print(categories.get(max(categories)))
